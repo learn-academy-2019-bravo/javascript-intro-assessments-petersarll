237d9fb200f4ad3e5de75af694150e35037ad842
Peter B. Sarll <petersarll@gmail.com>
2019-04-21 12:20:54 -0700

    finished assessment

--- js-intro.js
+++ js-intro.js
@@ -4,19 +4,18 @@
 
 var mantra = "Be the dev";
 
-// 1. Write the code that returns 'B' from mantra.
+// // 1. Write the code that returns 'B' from mantra.
+console.log(mantra.charAt(0));
+// // 2. Write the code that determines if there is a 'x' in mantra.
+ console.log(mantra.includes('x'))
+// // 3. Write the code that determines if there is a 'v' in mantra.
+ console.log(mantra.includes('v'))
+// // Stretch: Write the code that returns the position of 'v' in mantra.
+console.log(mantra.indexOf('v'))
+// // Consider the following statement:
 
-// 2. Write the code that determines if there is a 'x' in mantra.
-
-// 3. Write the code that determines if there is a 'v' in mantra.
-this is a test, added even more
-// Stretch: Write the code that returns the position of 'v' in mantra.
-
-
-// Consider the following statement:
-
-var codingIsEasy
-var imBored
+var codingIsEasy = true
+var imBored = true
 
 if (codingIsEasy && imBored) {
     console.log("I need more!")
@@ -24,12 +23,13 @@ if (codingIsEasy && imBored) {
     console.log("Steady as she goes")
 }
 
-// 4. Update the codingIsEasy and imBored variables above the statement to the values required to log "I need more!"
+// 4. Update the codingIsEasy and imBored variables above the statement
+//to the values required to log "I need more!"
 
 // Consider the following statement:
-
-var imLost
-var imFrustrated
+//
+var imLost = false
+var imFrustrated = false
 
 if (imLost || imFrustrated) {
     console.log('Break time!')
@@ -37,26 +37,93 @@ if (imLost || imFrustrated) {
     console.log('Keep coding!')
 }
 
-// 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
+// 5. Update the imLost and imFrustated variables above the statement to
+//the values required to log "Keep coding!"
 
-// 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
-
-// Stretch: Using a while loop.
-
+// 6a. Recall the mantra variable from the first question. Write the code
+//that logs each letter of mantra using a for loop.
+for (let i = 0; i < mantra.length ; i++){
+    console.log(mantra[i]);
+}
+// // Stretch: Using a while loop.
+//
+var count = 0
+while (count < mantra.length){
+    console.log(mantra[count]);
+    count++
+}
 // Super Stretch: Using forEach().
+
+newMantra = mantra.split('')
+
+function newArray(placeholder){
+    return console.log(placeholder);
+}
+newMantra.forEach(newArray)
 
 // Consider the following variable:
 
 var message = "thisisateststring"
 
-// 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+// // 7. Write a function that takes a string like message as an argument
+// //and returns the string without vowels.
+newMessage = message.split('')
+
+function removeVowelsString(string){
+    return string.replace(/[aeiou]/gi,'')
+}
+console.log(removeVowelsString(message))
+
 
 // Stretch: Update your function to throw an exception if the argument is not a string.
+function removeVowelsInArray(word){
+    let newArray = []
+    for (let i = 0; i < newMessage.length; i++){
+        if(newMessage[i] !== 'i' && newMessage[i] !== 'a' && newMessage[i] !== 'e' && newMessage[i] !== 'u' && newMessage[i] !== 'o' && typeof '' === 'string'){
+            newArray.push(word[i])
+    }
+} return newArray
+}
+console.log(removeVowelsInArray(message));
+
 
 // Super Duper Stretch: Update your function to throw a TypeError if the argument is not a string.
+// *** uncommenting the function below will not run the rest of the code***
+// *** uncommenting the function below will not run the rest of the code***
+// *** uncommenting the function below will not run the rest of the code***
+
+
+// function removeVowelsString(string){
+//     return string.replace(/[aeiou]/gi,'')
+// }
+// console.log(removeVowelsString(newMessage))
+// ^^ THIS ONE WILL CAUSE TYPE ERROR DUE TO newMessage BEING AN ARRAY
+
+
+// *** uncommenting the function above will not run the rest of the code***
+// *** uncommenting the function above will not run the rest of the code***
+// *** uncommenting the function above will not run the rest of the code***
+
 
 // Consider the following variable:
+var toonimals = [ {name: 'Itchy', animal: 'mouse'},
+{name: 'Stimpy', animal: 'cat'},
+{name: 'Daffy', animal: 'duck'},
+{name: 'Scratchy', animal: 'cat'},
+{name: 'Ren', animal: 'dog'},
+{name: 'Felix', animal: 'cat'}]
 
-var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
+// 8. Write a function that takes an array like toonimals and
+// returns an array with only the toons that are cats.
 
-// 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+function getCats(placeholder){
+    newArray = []
+    for (let i = 0; i < toonimals.length; i++){
+        if (toonimals[i].animal === 'cat') {
+            newArray.push(placeholder[i])
+        }
+    } return newArray
+}
+
+console.log(getCats(toonimals));
+// return the function to log the cats
--- js-intro.md
+++ js-intro.md
@@ -3,13 +3,18 @@
 Answer these questions, then follow the instructions to submit your first assessment!
 
 #### 1. What is a function? Why would you use one?
+// a function is a procedure we perform a task or return a specified value.
 
 #### 2. What is a method?
+a method is a function that is a property of an object
 
 #### 3. What are some examples of javascript string methods?
+charAt(), charCodeAt(), includes(), replace(), search(), split(), slice(), toLowerCase(), toString(),  
 
 #### 4. Explain the difference between accessor and mutator methods. Why would you use one over the other?
+A mutator is an operator that can manipulate the value of an array whereas an accessor will only receive the value you are looking for. If you need to manipulate & change the value of an array (for instance add 2 to 3) you could use the mutator method. if you just needed to receive the value without changing the value, then you would use the accessor.  
 
 #### 5. Explain the differences between console.log() and return. Why would you use one over the other?
-
+console log is a function to specifically print information to the console. This is great when trying to see mistakes in your code. Return changes the value if a call was made. once the function has stopped executing, it will return the unspecified value. it will not execute code on your console.
 #### 6. What is one thing we did in class this week that helped you learn the most?  
+Repetition. being able to do the same thing over and over again really helps. And reminding us that google is your friend. being reminded that you wont be able to remember everything and that searching is ok. Being able to search and use it in your code is valuable.
